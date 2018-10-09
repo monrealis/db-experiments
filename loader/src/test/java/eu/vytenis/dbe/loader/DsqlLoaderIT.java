8023b45a03b3977b971a7a7f3f2b86b726e1f1ed
Vytenis Bivainis <monrealis@gmail.com>
2018-10-10 00:44:26 +0300

    Experiment with JDBC

--- loader/src/test/java/eu/vytenis/dbe/loader/DsqlLoaderIT.java
+++ loader/src/test/java/eu/vytenis/dbe/loader/DsqlLoaderIT.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import eu.vytenis.dbe.dsql.model.Complex;
+import static eu.vytenis.dbe.dsql.client.ComplexDynamicSqlSupport.*;
+import static org.mybatis.dynamic.sql.SqlBuilder.*;
 
 class DsqlLoaderIT {
     private DsqlSession session = DsqlSession.vertica();
@@ -17,13 +19,29 @@ class DsqlLoaderIT {
     void deleteAll() {
         session.deleteAll();
     }
-    
+
     @Test
     void insert() {
         session.deleteAll();
+    }
+
+    @Test
+    void update() {
+        session.deleteAll();
+        session.complexes.insert(complex(1));
+        updateOne();
+    }
+
+    private void updateOne() {
         Complex c = new Complex();
         c.setId(1);
-        c.setName("Complex 1");
-        session.complexes.insert(c);
+        session.complexes.updateByExampleSelective(c).where(id, isEqualTo(1)).build().execute();
+    }
+
+    private Complex complex(int id) {
+        Complex c = new Complex();
+        c.setId(id);
+        c.setName("Complex " + id);
+        return c;
     }
 }
